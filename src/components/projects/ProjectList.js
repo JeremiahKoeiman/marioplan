80dae175d3569a279ea14ca5e024043900e630fb
JeremiahKoeiman <9001603@student.zadkine.nl>
2019-11-01 14:13:43 +0100

    Static prolectList & projectSummary modified to be modular

--- src/components/projects/ProjectList.js
+++ src/components/projects/ProjectList.js
@@ -1,31 +1,14 @@
 import React from 'react'
 
+import ProjectSummary from "./ProjectSummary";
+
 const ProjectsList = () => {
     return (
         <div className="project-list section">
-            <div className="card z-depth-2 project-summary">
-                <div className="card-content black-text">
-                    <span className="card-title">Project Title</span>
-                    <p>Posted by Jeremiah</p>
-                    <p className="grey-text">3rd September, 2am</p>
-                </div>
-            </div>
-
-            <div className="card z-depth-2 project-summary">
-                <div className="card-content black-text">
-                    <span className="card-title">Project Title</span>
-                    <p>Posted by Jeremiah</p>
-                    <p className="grey-text">3rd September, 2am</p>
-                </div>
-            </div>
-
-            <div className="card z-depth-2 project-summary">
-                <div className="card-content black-text">
-                    <span className="card-title">Project Title</span>
-                    <p>Posted by Jeremiah</p>
-                    <p className="grey-text">3rd September, 2am</p>
-                </div>
-            </div>
+            <ProjectSummary/>
+            <ProjectSummary/>
+            <ProjectSummary/>
+            <ProjectSummary/>
         </div>
     )
 }
