23cd11113a1f4e7898fc61e06433507eb8c21dc2
JeremiahKoeiman <9001603@student.zadkine.nl>
2019-11-05 10:51:40 +0100

    Dummy projects added

--- src/store/reducers/projectReducer.js
+++ src/store/reducers/projectReducer.js
@@ -1,5 +1,21 @@
 const initState = {
-
+    projects: [
+        {
+            id: 1,
+            title: 'help me find peach',
+            content: 'bla bla bla'
+        },
+        {
+            id: 2,
+            title: 'collect all the stars',
+            content: 'bla bla bla'
+        },
+        {
+            id: 2,
+            title: 'egg hunt with yoshi',
+            content: 'bla bla bla'
+        }
+    ]
 }
 
 const projectReducer = (state = initState, action) => {
