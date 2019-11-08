4598dbc45d4de2708a02c2b8c90fbdb77e13bc87
JeremiahKoeiman <9001603@student.zadkine.nl>
2019-11-08 10:42:08 +0100

    Release 1.0.0 released

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
 import { Provider } from "react-redux";
-import {ReactReduxFirebaseProvider} from "react-redux-firebase";
-import createReduxStore from "./store/createReduxStore";
-import fbConfig from './config/fbConfig'
+import thunk from "redux-thunk";
+import {createFirestoreInstance, getFirestore, reduxFirestore} from "redux-firestore";
+import {ReactReduxFirebaseProvider, getFirebase} from "react-redux-firebase";
+import firebase from "firebase/app";
 
 import Navbar from "./components/layout/Navbar";
 import Dashboard from "./components/dashboard/Dashboard";
@@ -11,14 +12,44 @@ import ProjectDetails from "./components/projects/ProjectDetails";
 import SignIn from "./components/auth/SignIn";
 import SignUp from "./components/auth/SignUp";
 import CreateProject from "./components/projects/CreateProject";
-import * as firebase from "firebase";
+// eslint-disable-next-line
+import fbConfig from './config/fbConfig' // --> if you remove this, it won't work
+import {applyMiddleware, compose, createStore} from "redux";
+import rootReducer from "./store/reducers/rootReducer";
 
-const store = createReduxStore()
+firebase.firestore();
+
+const rrfConfig = {
+    userProfile: 'users',
+    useFirestoreForProfile: true
+}
+
+const middleware = [
+    thunk.withExtraArgument({ getFirestore }),
+    thunk.withExtraArgument({ getFirebase }),
+]
+
+
+const store = createStore(
+    rootReducer,
+    compose(
+        applyMiddleware(...middleware),
+        reduxFirestore(firebase)
+    ),
+);
+
+const rrfProps = {
+   firebase,
+   config: rrfConfig,
+   dispatch: store.dispatch,
+   createFirestoreInstance
+ }
+
 
 function App() {
     return (
         <Provider store={store}>
-            <ReactReduxFirebaseProvider firebase={firebase} config={fbConfig} dispatch={store.dispatch}>
+            <ReactReduxFirebaseProvider {...rrfProps}>
                 <Router>
                     <div className="App">
                         <Navbar/>
